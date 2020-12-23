35f40644be20277e509f6967b422656c6142e694
Isaac Marovitz <42140194+IsaacMarovitz@users.noreply.github.com>
2020-12-23 00:54:27 +0000

    Army Editor Script

---
+++ Assets/Scripts/Units/Editor/ArmyEditor.cs
@@ -0,0 +1,20 @@
+using UnityEditor;
+
+[CustomEditor(typeof(Army))]
+public class ArmyEditor : UnitEditor {
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+
+        SerializedObject so = new SerializedObject(target);
+        Army army = (Army)target;
+
+        EditorGUILayout.Space(10);
+        EditorGUILayout.LabelField("Reduced Movement", EditorStyles.boldLabel);
+        army.reducedMoveDistance = EditorGUILayout.IntSlider("Reduced Move Distance", army.reducedMoveDistance, 0, army.maxMoves);
+        army.isMoveDistanceReduced = EditorGUILayout.Toggle("Is Move Distance Reduced", army.isMoveDistanceReduced);
+
+        Others();
+
+        so.ApplyModifiedProperties();
+    }
+}
